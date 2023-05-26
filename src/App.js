003238bfbd3c62699c09dee6d7af8c31d4d9ba38
javier <javier.ramon@eyiroo.com>
2023-05-26 02:16:24 -0500

    "Creacion del features"

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Components/Head/Header'
 import './App.css'
 import Home from './Components/Hero/Home'
+import Features from './Components/Features/Features'
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <Features />
     </>
   ) 
     
