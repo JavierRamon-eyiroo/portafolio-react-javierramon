afdd7d4e9bc3ee29b2dfdce62fef49f8131c6ca0
javier <javier.ramon@eyiroo.com>
2023-05-26 01:58:07 -0500

    "Creacion del Home"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Header from './Components/Head/Header'
 import './App.css'
+import Home from './Components/Hero/Home'
 
 const App = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  ) 
+    
 }
 
 export default App
